2624f53142de71c39fc67a4fcb990e52b5b70527
julien-guillan <guillan.julien@live.com>
2022-01-31 16:14:27 +0100

    Initial Commit

---
+++ instagram_like/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagram_like
+//
+//  Created by Julien Guillan on 31/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram_like/instagram_likeApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_likeApp.swift
+//  instagram_like
+//
+//  Created by Julien Guillan on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct instagram_likeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
